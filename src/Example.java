aba574945c0945f1e8b0b51fdf871b8d13e1d7c4
morzylek <morzylek@o2.pl>
2015-11-21 15:20:30 +0100

    Cut Code Main.java and added Example.java

---
+++ src/Example.java
@@ -0,0 +1,14 @@
+
+public class Example {
+	private int myIntegerField;
+	
+	public void display() {
+		System.out.println("My Field: " + myIntegerField);
+		
+	}
+	
+	public Example() {
+		myIntegerField = 0;
+	}
+
+}
